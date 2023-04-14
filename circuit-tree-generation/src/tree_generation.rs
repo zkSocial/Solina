16e95bf90c80aa63c2304d07a0472d18255d8618
jorgeantonio21 <matroid@outlook.com>
2023-04-14 11:55:07 +0200

    add verify merkle tree circuit targets test

--- circuit-tree-generation/src/tree_generation.rs
+++ circuit-tree-generation/src/tree_generation.rs
@@ -11,8 +11,8 @@ use plonky2::{
 
 pub trait MerkleTreeGeneration<F: RichField + Extendable<D>, const D: usize> {
     type Hasher;
-    fn build_merkle_root_target(
-        builder: &mut CircuitBuilder<F, D>,
+    fn add_merkle_root_target(
+        &mut self,
         targets: Vec<Vec<Target>>,
         to_extend_target: Target,
     ) -> HashOutTarget;
@@ -22,25 +22,25 @@ impl<F: RichField + Extendable<D>, const D: usize> MerkleTreeGeneration<F, D>
     for CircuitBuilder<F, D>
 {
     type Hasher = PoseidonHash;
-    fn build_merkle_root_target(
-        builder: &mut CircuitBuilder<F, D>,
+    fn add_merkle_root_target(
+        &mut self,
         targets: Vec<Vec<Target>>,
         to_extend_target: Target,
     ) -> HashOutTarget {
         // extend `targets` to a length of power of two vector
-        let targets = extend_targets_to_power_of_two(builder, targets, to_extend_target);
+        let targets = extend_targets_to_power_of_two(self, targets, to_extend_target);
         // build the merkle tree root target
         let merkle_tree_height = targets.len().ilog2();
         let mut tree_hash_targets = vec![];
         for i in 0..targets.len() {
-            let hash_target = builder.hash_or_noop::<Self::Hasher>(targets[i].clone());
+            let hash_target = self.hash_or_noop::<Self::Hasher>(targets[i].clone());
             tree_hash_targets.push(hash_target);
         }
         let mut current_tree_height_index = 0;
         for height in 0..merkle_tree_height {
             // TODO: do we want to loop over all the height, or until cap(1) ?
             for i in 0..(1 << merkle_tree_height - height) {
-                let hash_targets = builder.hash_n_to_hash_no_pad::<Self::Hasher>(
+                let hash_targets = self.hash_n_to_hash_no_pad::<Self::Hasher>(
                     [
                         tree_hash_targets[i as usize].elements.clone(),
                         tree_hash_targets[i as usize + 1].elements.clone(),
