a4cf30f7b6d9be89cc7685b705e1fbe1f91df23d
jorgeantonio21 <matroid@outlook.com>
2023-08-22 09:59:46 +0100

    clean up serde implementation dependencies and tests

--- solina-service/src/types.rs
+++ solina-service/src/types.rs
@@ -1,3 +1,12 @@
+use crate::intents::Intent;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct IntentRequest {
     intent: Intent,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct IntentResponse {
+    is_sucess: bool,
 }
