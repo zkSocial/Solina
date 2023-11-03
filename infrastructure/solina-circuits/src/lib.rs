25817b1e974c646a4d997274907837274d824ff5
jorgeantonio21 <matroid@outlook.com>
2023-11-03 19:07:16 +0000

    refactor use of u32 targets directly for circuit builder

--- infrastructure/solina-circuits/src/lib.rs
+++ infrastructure/solina-circuits/src/lib.rs
@@ -1,13 +1,2 @@
-use plonky2::{
-    field::goldilocks_field::GoldilocksField, hash::poseidon::PoseidonHash,
-    plonk::config::PoseidonGoldilocksConfig,
-};
-
 pub mod match_circuit;
 pub mod solver_circuit;
-
-pub const D: usize = 2;
-
-pub type F = GoldilocksField;
-pub type C = PoseidonGoldilocksConfig;
-pub type H = PoseidonHash;
--- infrastructure/solina-circuits/src/match_circuit.rs
+++ infrastructure/solina-circuits/src/match_circuit.rs
@@ -1,12 +1,12 @@
-use crate::{C, D, F};
 use plonky2::{
-    iop::target::Target,
+    field::extension::Extendable,
+    hash::hash_types::RichField,
     plonk::{circuit_builder::CircuitBuilder, circuit_data::CircuitConfig},
 };
+use plonky2_u32::gadgets::arithmetic_u32::{CircuitBuilderU32, U32Target};
 use solina::solver::Match;
-use zktree::circuit_compiler::CircuitCompiler;
 
-pub type TokenAddressTargets = [Target; 4];
+pub type TokenAddressTargets = [U32Target; 4];
 
 pub struct MatchTargets {
     intent_a_quote_token_targets: TokenAddressTargets,
@@ -15,41 +15,69 @@ pub struct MatchTargets {
     intent_b_quote_token_targets: TokenAddressTargets,
 }
 
-pub struct MatchCircuitData {
+pub struct MatchCircuitData<F: RichField + Extendable<D>, const D: usize> {
     circuit_builder: CircuitBuilder<F, D>,
     targets: MatchTargets,
 }
 
-pub fn generate_match_circuit(match_instance: Match) -> (CircuitBuilder<F, D>, MatchTargets) {
+pub fn generate_match_circuit<F, const D: usize>(match_instance: Match) -> MatchCircuitData<F, D>
+where
+    F: RichField + Extendable<D>,
+{
     let mut circuit_builder =
         CircuitBuilder::<F, D>::new(CircuitConfig::standard_recursion_zk_config());
 
     // 1. Verify that both intents have appropriate token addresses
-    let intent_a_quote_token_targets = circuit_builder.add_virtual_targets(4);
-    let intent_b_base_token_targets = circuit_builder.add_virtual_targets(4);
+    let intent_a_quote_token_targets = circuit_builder.add_virtual_u32_targets(4);
+    let intent_b_base_token_targets = circuit_builder.add_virtual_u32_targets(4);
 
     (0..4).for_each(|i| {
-        circuit_builder.connect(
+        circuit_builder.connect_u32(
             intent_a_quote_token_targets[i],
             intent_b_base_token_targets[i],
         )
     });
 
-    let intent_a_base_token_targets = circuit_builder.add_virtual_targets(4);
-    let intent_b_quote_token_targets = circuit_builder.add_virtual_targets(4);
+    let intent_a_base_token_targets = circuit_builder.add_virtual_u32_targets(4);
+    let intent_b_quote_token_targets = circuit_builder.add_virtual_u32_targets(4);
 
-    circuit_builder.connect(intent_a_base_token_targets, intent_b_quote_token_targets);
+    intent_a_base_token_targets
+        .iter()
+        .zip(intent_b_quote_token_targets)
+        .for_each(|(a, b)| circuit_builder.connect_u32(*a, b));
 
     // 2. Verify that the amount being swapped does not exceed the desired one, for each intent.
-    let intent_a_quote_amount_targets =
-        circuit_builder.add_virtual_biguint_target(match_instance.intent_a().inputs.quote_amount);
-    let intent_b_quote_amount_targets =
-        circuit_builder.add_virtual_biguint_target(match_instance.intent_b().inputs.quote_amount);
+    let intent_a_quote_amount_targets = circuit_builder.add_virtual_u32_targets(
+        match_instance
+            .intent_a()
+            .inputs
+            .quote_amount
+            .to_u32_digits()
+            .len(),
+    );
+    let intent_b_quote_amount_targets = circuit_builder.add_virtual_u32_targets(
+        match_instance
+            .intent_b()
+            .inputs
+            .quote_amount
+            .to_u32_digits()
+            .len(),
+    );
 
-    let match_intent_a_amount_targets = circuit_builder
-        .add_virtual_biguint_target(match_instance.swapped_amount().token_a_amount());
-    let match_intent_b_amount_targets = circuit_builder
-        .add_virtual_biguint_target(match_instance.swapped_amount().token_b_amount());
+    let match_intent_a_amount_targets = circuit_builder.add_virtual_u32_targets(
+        match_instance
+            .swapped_amount()
+            .token_a_amount()
+            .to_u32_digits()
+            .len(),
+    );
+    let match_intent_b_amount_targets = circuit_builder.add_virtual_u32_targets(
+        match_instance
+            .swapped_amount()
+            .token_b_amount()
+            .to_u32_digits()
+            .len(),
+    );
     // TODO: check for the inequalities
 
     todo!()
